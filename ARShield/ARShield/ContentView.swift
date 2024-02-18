ead99e1fe4e07a5d453d1f575dbe146ecf18dda8
avetian7 <avetian7@uw.edu>
2024-02-17 20:00:17 -0800

    dev: adding boxes

--- ARShield/ARShield/ContentView.swift
+++ ARShield/ARShield/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
+
 
 struct ContentView : View {
     var body: some View {
@@ -14,27 +16,34 @@ struct ContentView : View {
     }
 }
 
+
 struct ARViewContainer: UIViewRepresentable {
     
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
+   func makeUIView(context: Context) -> ARView {
+       
+       let arView = ARView(frame: .zero)
+      
+      let bottomCube = createCube(color: UIColor.blue.withAlphaComponent(0.8))
+      let bottomAnchor = AnchorEntity(world: [0.0, 0.0, 0])
+       bottomAnchor.addChild(bottomCube)
+       arView.scene.anchors.append(bottomAnchor)
+   
+      let topCube = createCube(color: UIColor.red.withAlphaComponent(1.0))
+      let topAnchor = AnchorEntity(world: [0, 0.05, 0])
+       topAnchor.addChild(topCube)
+       arView.scene.anchors.append(topAnchor)
 
-        // Create a cube model
-        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-        let model = ModelEntity(mesh: mesh, materials: [material])
+       return arView
+  }
 
-        // Create horizontal plane anchor for the content
-        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-        anchor.children.append(model)
+  // Create a cube model
+  func createCube(color: UIColor) -> Entity {
+      let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+      let material = SimpleMaterial(color: color, roughness: 0.15, isMetallic: true)
+      let model = ModelEntity(mesh: mesh, materials: [material])
+      return model
+  }
 
-        // Add the horizontal plane anchor to the scene
-        arView.scene.anchors.append(anchor)
-
-        return arView
-        
-    }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
